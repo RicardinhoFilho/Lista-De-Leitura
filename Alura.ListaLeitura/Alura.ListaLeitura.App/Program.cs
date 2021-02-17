7c42f08fe492394e2a1d8c7c95cf1a2c0fdd014d
Ricardo <ricardinhogiasson16@gmail.com>
2021-02-17 11:28:43 -0300

    First Commit

---
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Program.cs
@@ -0,0 +1,39 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;//Diretiva using para utilizarmos interface IWebHost
+using System;
+
+namespace Alura.ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            try
+            {
+                IWebHost host = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseStartup<Startup>()
+                    .Build();
+                host.Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(" ERROS -> "+ e.Message+ "\n" + e.StackTrace);
+            }
+            
+
+
+            //ImprimeLista(_repo.ParaLer);
+            //ImprimeLista(_repo.Lendo);
+            //ImprimeLista(_repo.Lidos);
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
